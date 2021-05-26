7d87c9ac0eb0ea26fd26b66acd105f9889c89889
defi-dev <defi.dev@protonmail.com>
2021-05-26 21:19:40 +0300

    debug getRoutersToPoke

--- web3/index.ts
+++ web3/index.ts
@@ -207,11 +207,10 @@ class PowerOracleWeb3 implements IPowerOracleWeb3 {
   async getRoutersToPoke() {
     const timestamp = await this.getTimestamp();
     return pIteration.filter(this.httpRouterContracts, async (routerContract) => {
-      console.log('routerContract.methods', routerContract.methods);
       let [{min: minReportInterval, max: maxReportInterval}, lastRebalancedAt, reserveStatus] = await Promise.all([
         this.httpPokerContract.methods.getMinMaxReportIntervals(routerContract._address).call(),
-        routerContract.methods.lastRebalancedAt().then(r => utils.normalizeNumber(r)),
-        routerContract.methods.getReserveStatusForStakedBalance()
+        routerContract.methods.lastRebalancedAt().call().then(r => utils.normalizeNumber(r)),
+        routerContract.methods.getReserveStatusForStakedBalance().call()
       ]);
       minReportInterval = utils.normalizeNumber(minReportInterval);
       maxReportInterval = utils.normalizeNumber(maxReportInterval);
