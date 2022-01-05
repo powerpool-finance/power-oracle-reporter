7b2aa1b9e590ee1bd7d3dada54aa4fbf54a7551e
defi-dev <defi.dev@protonmail.com>
2022-01-05 14:29:09 +0800

    increase getReadyToExecuteRounds blocks

--- web3/index.ts
+++ web3/index.ts
@@ -345,7 +345,7 @@ class PowerOracleWeb3 implements IPowerOracleWeb3 {
   }
 
   async getReadyToExecuteRounds() {
-    const fromBlock = (await this.getCurrentBlock()) - 50000;
+    const fromBlock = (await this.getCurrentBlock()) - 100000;
     const roundInited = await this.httpIndicesZapContract.getPastEvents('InitRound', { fromBlock });
     const readyToExecute = [];
     await pIteration.forEachSeries(_.chunk(roundInited, 10), (chunk) => {
